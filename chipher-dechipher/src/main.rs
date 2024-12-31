6f36590c507fb715694a0e86b7218c5be1f0b634
Santosh Khanal <117138852+santoshkhanal12@users.noreply.github.com>
2024-12-31 13:39:56 +0545

    Added id else condition
    Added id else condition

--- chipher-dechipher/src/main.rs
+++ chipher-dechipher/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello,1111 world!");
+use std::io;       //Uses standard input/output operation
+fn main() {                           //This is the main function
+    let mut  mode = String::new();
+    println!("Please enter the mode (Chipier/Dichipher)");
+    io::stdin().read_line(&mut mode)
+    .expect("Failed to read line");   
+    mode=mode.trim().to_lowercase();
+    if mode == "chipher"{
+        println!("You entered Chiphered mode");
+    }
+    else if mode == "dechipher"{
+        println!("You entered dichiphered mode");
+    }
+    else {
+        println!("You entered the wrong mode")
+    }
 }
